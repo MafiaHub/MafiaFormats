c5485c6bafaf2794225ee621a7dd2ef7fbc9e6b4
Dominik Madarász <zaklaus@outlook.com>
2019-01-30 19:33:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mafia formats
+This repository contains known reverse-engineered binary format specifications with sample parsers and documentation.
+
+## How to use
+To compile the documentation, you need to make sure [go-specgen](https://github.com/zaklaus/go-specgen) is installed and within your `$PATH` variable.
+
+### Usage: Documentation
+Simply run `compile.sh` to generate the documentation.
+
+### Usage: Parser utility
+Some formats might have sample read/write tools written, so you can easily read and dump the binary data in various formats. Check out the `parsers` directory.
+
+## License
+This repository is licensed under Apache 2.0 license. See `COPYING.MD` for more information.
+
---
+++ specs/check_bin.gspec.go
@@ -0,0 +1,42 @@
+package checkbin
+
+/*
+@enum PointType::
+-> Pedestrian = 0x1;
+-> AI = 0x2;
+-> Vehicle = 0x4;
+-> Tram_Station = 0x8;
+-> Special = 0x10;
+*/
+
+/*
+@enum LinkType::
+-> Pedestrian = 1;
+-> AI = 2;
+-> TrainsAndSalinas_Forward = 4;
+-> TrainsAndSalinas_Reverse = 0x8400;
+-> Other = 0x1000;
+*/
+
+// Header specifies the file's magic constant and number of navigation points
+type Header struct {
+	Magic     uint32
+	NumPoints uint32
+}
+
+// Point specifies an actual nav node with enter/leave links and properties
+type Point struct {
+	Type       uint16
+	ID         uint16
+	AreaSize   uint16
+	Unk        [10]uint8 /* @plain Unknown values */
+	EnterLinks uint8
+	ExitLinks  uint8
+}
+
+// Link specifies the connection between points
+type Link struct {
+	TargetPoint uint16
+	LinkType    uint16
+	Unk         float32
+}
